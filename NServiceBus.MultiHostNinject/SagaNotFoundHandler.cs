ac4b8ffe71e4658f16b896c5c2495ae50307f4fb
Ramon Smits <ramon.smits@gmail.com>
2018-05-14 13:08:43 +0200

    Adding SagaNotFoundHandler to enforce an exception if a saga instance is not found.

---
+++ NServiceBus.MultiHostNinject/SagaNotFoundHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Sagas;
+
+class SagaNotFoundHandler : IHandleSagaNotFound
+{
+    public Task Handle(object message, IMessageProcessingContext context)
+    {
+        throw new Exception("Saga not found");
+    }
+}
